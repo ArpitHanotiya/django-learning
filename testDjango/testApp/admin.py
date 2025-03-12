28589291ba49f302306f561cc2069d26c1be858e
Arpit Hanotiya <ArpitHanotiya>
2025-03-12 18:49:33 +0530

    Django Relationship Models

--- testDjango/testApp/admin.py
+++ testDjango/testApp/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import testAppVariety
+from .models import testAppVariety, testAppReview, store, testAppCertificate
 
 # Register your models here.
-admin.site.register(testAppVariety)
+class testAppReviewInLine(admin.TabularInline):
+    model = testAppReview
+    extra = 2
+
+class testAppVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [testAppReviewInLine]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('app_varities', )
+
+class testAppCertificateAdmin(admin.ModelAdmin):
+    list_display = ('app', 'certificate_number')
+
+admin.site.register(testAppVariety, testAppVarietyAdmin)
+admin.site.register(store, storeAdmin)
+admin.site.register(testAppCertificate, testAppCertificateAdmin)
--- testDjango/testApp/models.py
+++ testDjango/testApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class testAppVariety(models.Model):
@@ -18,3 +19,36 @@ class testAppVariety(models.Model):
     
     def __str__(self):
         return self.name
+    
+# one to many -> Foreign key
+class testAppReview(models.Model):
+    app = models.ForeignKey(testAppVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.app.name}'
+
+# related name -> what is my name in other table
+
+# many to many
+class store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    app_varities = models.ManyToManyField(testAppVariety, related_name='stores')
+
+    def __str__(self):
+        return self.name 
+
+
+# one to one
+class testAppCertificate(models.Model):
+    app = models.OneToOneField(testAppVariety, on_delete=models.CASCADE, related_name='certificate') 
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.name.app}'
